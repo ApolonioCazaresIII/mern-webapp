458ef447cdc7cca1f6a26368ff4986a2ca66ed38
BuildTools <acazares@umich.edu>
2020-06-28 12:20:34 -0400

    Place mongo uri in env for safety

--- middleware/database.js
+++ middleware/database.js
@@ -1,8 +1,7 @@
 import { MongoClient } from 'mongodb';
 import nextConnect from 'next-connect';
 
-const connectionString =
-  'mongodb+srv://polocaz:161312@maincluster1-danyu.mongodb.net/test?retryWrites=true&w=majority';
+const connectionString = process.env.MONGO_URI;
 const client = new MongoClient(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
